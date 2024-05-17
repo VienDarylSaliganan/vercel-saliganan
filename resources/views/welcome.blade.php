064f02d511dff9fad981722979b27a3524bb5822
Vien Daryl Saliganan <viendaryldaryl@gmail.com>
2024-05-17 14:57:57 +0800

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+    <!-- Bootstrap + Meyawo main styles -->
+    <link rel="stylesheet" href="assets/css/meyawo.css">
+    <!-- font icons -->
+
+
+
+
+</head>
+<!-- page header -->
+<header id="home" class="header">
+    <div class="overlay"></div>
+    <div class="header-content container">
+        <h1 class="header-title">
+            <span class="up">HI!</span>
+            <span class="down">I am Vien Daryl Saliganan</span>
+        </h1>
+        <p class="header-subtitle">Web Developer</p>
+
+
+    </div>
+</header><!-- end of page header -->
+
+<!-- about section -->
+<section class="section pt-0" id="about">
+    <!-- container -->
+    <div class="container text-center">
+        <!-- about wrapper -->
+        <div class="about">
+            <div class="about-img-holder">
+                <img src="assets/imgs/man.png" class="about-img"
+                    alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page">
+            </div>
+            <div class="about-caption">
+                <p class="section-subtitle">Who Am I ?</p>
+                <h2 class="section-title mb-3">About Me</h2>
+                <p>
+                    I am a Web Developer, I am currently a student at the Saint Louis College taking up Bachelor of
+                    Science in Information Technology.
+                    <br>
+                </p>
+
+            </div>
+        </div><!-- end of about wrapper -->
+    </div><!-- end of container -->
+</section> <!-- end of about section -->
+
+<!-- service section -->
+<section class="section" id="service">
+    <div class="container text-center">
+        <h6 class="section-title mb-6">What Do I Do</h6>
+        <p></p>
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-6 col-lg-3">
+                <div class="service-card">
+                    <div class="body">
+                        <img src="assets/imgs/toolbox.svg"
+                            alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"
+                            class="icon">
+                        <h6 class="title">Back-end developer</h6>
+                        <p class="subtitle">I like to code things from scratch, and make it a functional web.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="service-card">
+                    <div class="body">
+                        <img src="assets/imgs/pencil-case.svg"
+                            alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"
+                            class="icon">
+                        <h6 class="title">Designer</h6>
+                        <p class="subtitle">I value simple content structure, clean design patterns, and thoughtful
+                            interactions.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="service-card">
+                    <div class="body">
+                        <img src="assets/imgs/responsive.svg"" alt=" Download free bootstrap 4 landing page, free
+                            boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1
+                            templates, meyawo Landing page" class="icon">
+                        <h6 class="title">Front-end Developer </h6>
+                        <p class="subtitle">I enjoy bringing ideas to life in the browser.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div><!-- end of row -->
+    </div>
+
+
+
+
+    <!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap 3 affix -->
+    <script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- Meyawo js -->
+    <script src="assets/js/meyawo.js"></script>
+
+    </body>
+
+</html>
